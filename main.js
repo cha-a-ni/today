d0b0ec49b25d0c0ec694e40fce4e0d32e1604315
chanhee <chanhee0149@gmail.com>
2024-10-02 10:00:31 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+    // TODO
+    return -1;
+}
+
+function add_from(a, b) {
+    // TODO
+    return -1;
+}
+
+let in1 = 5;
+let in2 = 10;
+
+console.log("add result: " + add(in1, in2));
+console.log("add_from result: " + add_from(in1, in2));
